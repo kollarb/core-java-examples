2e3abff59145da65280ef62b59374c14a0770f19
bharath kollareddy <bkollareddy@gmail.com>
2017-02-18 19:45:32 -0500

    added comments

--- src/core/java/basics/ObjectExample.java
+++ src/core/java/basics/ObjectExample.java
@@ -2,19 +2,26 @@ package core.java.basics;
 
 import java.util.Scanner;
 
+//class
 public class ObjectExample {
 	
 	public static void main(String[] args) {
+		//object
 		Scanner scan= new Scanner(System.in);
 		System.out.println("Enter value for i::");
+		//variable
 		int i= scan.nextInt();
 		System.out.println("Enter value for j::");
+		//variable
 		int j= scan.nextInt();
+		
+		//object
 		ObjectExample oe= new ObjectExample();
 		int result=oe.sum(i,j);
 		System.out.println("sum::"+result);
 	}
 
+	//method
 	private int sum(int i, int j) {
 		return i+j;
 	}
